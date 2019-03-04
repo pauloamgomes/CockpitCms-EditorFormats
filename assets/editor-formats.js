fc3dfc822453666eb9f7547b79fdece49dd0e284
Raffael <github@raffaeljesche.de>
2019-03-04 12:44:36 +0100

    added i18n support for tinymce

--- assets/editor-formats.js
+++ assets/editor-formats.js
@@ -11,11 +11,19 @@ App.$(document).on("init-wysiwyg-editor", function(e, editor) {
     editor.settings.format
   ], "access").then(function(data) {
     if (data && data.result) {
+
+      var options,
+          lang = editor.settings.language || App.$data.user.i18n || document.documentElement.getAttribute('lang') || 'en';
+
       options = data.result;
       options.branding = false;
       options.selector = "#" + editor.id;
       options.modified = true;
       options.setup = editor.settings.setup;
+
+      options.language = lang;
+      options.language_url = lang == 'en' ? '' : App.route('/config/cockpit/i18n/tinymce/'+lang+'.js');
+
       tinymce.EditorManager.execCommand("mceRemoveEditor", false, editor.id);
       new tinymce.Editor(editor.id, options, tinymce.EditorManager).render();
     }
